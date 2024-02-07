7e2e3eb70e5b8569a0a7aa972f0f6550e27a028b
nihil21 <mattia.orlandi21@gmail.com>
2024-02-07 17:15:40 +0100

    Added experiments with armband

--- emgkit/__init__.py
+++ emgkit/__init__.py
@@ -16,10 +16,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from . import decomposition, plotting, preprocessing, rls, spike_stats, utils
+from . import decomposition, features, plotting, preprocessing, rls, spike_stats, utils
 
 __all__ = [
     "decomposition",
+    "features",
     "plotting",
     "preprocessing",
     "rls",
